673750dc1211ae91b02cd16ade07a908b3787e71
DonSergio91 <serjuck@gmail.com>
2020-07-12 13:39:27 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -34,7 +34,19 @@ $(function(){
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active');
     })
-       
+
+
+    
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+    
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+
+
+
 
     var mixer = mixitup('.products__inner-box');
 
