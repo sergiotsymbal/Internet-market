7ba9c284d62f12d54339b87c27219ff7bd2d6808
DonSergio91 <serjuck@gmail.com>
2020-07-30 15:39:19 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,32 @@ $(function(){
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 1441,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                }
+            },
+            {
+              breakpoint: 801,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                }
+            },
+        ]
     });
 
     $(".js-range-slider").ionRangeSlider({
@@ -27,11 +52,11 @@ $(function(){
     $('.icon-th-list').on('click', function(){
         $('.product__item').addClass('list');
         $('.icon-th-list').addClass('active');
-        $('.icon-th-large').removeClass('active');
+        $('.icon-th-large1').removeClass('active');
     });
-    $('.icon-th-large').on('click', function(){
+    $('.icon-th-large1').on('click', function(){
         $('.product__item').removeClass('list');
-        $('.icon-th-large').addClass('active');
+        $('.icon-th-large1').addClass('active');
         $('.icon-th-list').removeClass('active');
     })
 
@@ -45,6 +70,7 @@ $(function(){
             return false;
         });
 
+    $('input[type="file"], select').styler();
     
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
